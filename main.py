8d4d6f9face678c840ec2127f1c29376cc315894
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2026-07-01 11:48:17 -0300

    Refatora função main para incluir a construção do modelo e a montagem do sistema estático

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from pathlib import Path
 
+from src.fem_buckling.assembler import StaticAssembler
+from src.fem_buckling.model_builder import ModelBuilder
 from src.fem_buckling.parser import InputReader
 
 
 def main():
     input_reader = InputReader()
     input_reader.read(Path("src/examples/ex01.ie"))
-    input_reader.parse()
+    input_data = input_reader.parse()
+
+    builder = ModelBuilder()
+    model = builder.build(input_data)
+
+    static_assembler = StaticAssembler(model)
+    static_system = static_assembler.get_partitioned_system()
+    print(f"Partitioned System: {static_system}")
 
 
 if __name__ == "__main__":
