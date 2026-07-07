5f1e870576da3c14ef54e04ee37d57b90182f250
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2026-07-07 14:23:47 -0300

    feat: Add BucklingAssembler class for global stiffness matrix assembly and partitioning

--- src/fem_buckling/assembler.py
+++ src/fem_buckling/assembler.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from src.fem_buckling.boundary_condition import BoundaryConditionType, SpringDirection
 from src.fem_buckling.model_builder import Model
+from src.fem_buckling.result import AxialResults
 
 
 @dataclass(frozen=True)
@@ -14,6 +15,14 @@ class PartitionedSystem:
     K_cc: np.ndarray
     f_f: np.ndarray
     f_c: np.ndarray
+    free_dofs: np.ndarray
+    constrained_dofs: np.ndarray
+
+
+@dataclass(frozen=True)
+class PartitionedBucklingSystem:
+    K_ff: np.ndarray
+    Kg_ff: np.ndarray
     free_dofs: np.ndarray
     constrained_dofs: np.ndarray
 
@@ -110,6 +119,110 @@ class AxialAssembler:
             K_cc=K_cc,
             f_f=f_f,
             f_c=f_c,
+            free_dofs=free_dofs,
+            constrained_dofs=constrained_dofs,
+        )
+
+
+class BucklingAssembler:
+    def __init__(self, model: Model, axial_results: AxialResults):
+        self.model = model
+        self.axial_results = axial_results
+        self.nodes = model.mesh.nodes
+        self.elements = model.mesh.elements
+        self.boundary_conditions = model.load_case.boundary_conditions
+        self.spring_supports = model.load_case.spring_supports
+        self.axial_forces = axial_results.axial_forces
+        self.node_dofs_mapping = self.map_node_dofs()
+
+    def map_node_dofs(self):
+        dof_mapping = {}
+        for i, node in enumerate(self.nodes):
+            dof_mapping[node.id] = (2 * i, 2 * i + 1)
+        return dof_mapping
+
+    def assemble_global_elastic_stiffness_matrix(self):
+        K = np.zeros((2 * len(self.nodes), 2 * len(self.nodes)))
+
+        for element in self.elements:
+            node_1, node_2 = element.nodes
+            dofs = [
+                self.node_dofs_mapping[node_1.id][0],
+                self.node_dofs_mapping[node_1.id][1],
+                self.node_dofs_mapping[node_2.id][0],
+                self.node_dofs_mapping[node_2.id][1],
+            ]
+            k_local = element.calculate_transverse_stiffness_matrix()
+            K[np.ix_(dofs, dofs)] += k_local
+
+        transverse_springs = [
+            spring
+            for spring in self.spring_supports
+            if spring.direction == SpringDirection.TRANSVERSE
+        ]
+        rotational_springs = [
+            spring
+            for spring in self.spring_supports
+            if spring.direction == SpringDirection.ROTATIONAL
+        ]
+        for spring in transverse_springs:
+            dof = self.node_dofs_mapping[spring.node.id][0]
+            K[dof, dof] += spring.stiffness
+        for spring in rotational_springs:
+            dof = self.node_dofs_mapping[spring.node.id][1]
+            K[dof, dof] += spring.stiffness
+
+        return K
+
+    def assemble_global_geometric_stiffness_matrix(self):
+        Kg = np.zeros((2 * len(self.nodes), 2 * len(self.nodes)))
+
+        for element in self.elements:
+            node_1, node_2 = element.nodes
+            dofs = [
+                self.node_dofs_mapping[node_1.id][0],
+                self.node_dofs_mapping[node_1.id][1],
+                self.node_dofs_mapping[node_2.id][0],
+                self.node_dofs_mapping[node_2.id][1],
+            ]
+            reference_axial_force = (-1) * next(
+                (f.force for f in self.axial_forces if f.element.id == element.id)
+            )  # convention: positive axial force is compression, negative is tension
+            kg_local = element.calculate_geometric_stiffness_matrix(
+                axial_reference_force_positive_in_compression=reference_axial_force
+            )
+            Kg[np.ix_(dofs, dofs)] += kg_local
+
+        return Kg
+
+    def get_partitioned_system(self) -> PartitionedBucklingSystem:
+        K = self.assemble_global_elastic_stiffness_matrix()
+        Kg = self.assemble_global_geometric_stiffness_matrix()
+
+        all_dofs = np.array(
+            [dof for node in self.nodes for dof in self.node_dofs_mapping[node.id]]
+        )
+        constrained_dofs = np.array(
+            [
+                dof
+                for bc in self.boundary_conditions
+                for dof in self.node_dofs_mapping[bc.node.id]
+                if (bc.bc_transverse == BoundaryConditionType.FIXED)
+                or (bc.bc_rotational == BoundaryConditionType.FIXED)
+            ]
+        )
+
+        if len(constrained_dofs) == 0:
+            raise ValueError("Nenhum GDL restrito encontrado.")
+
+        free_dofs = np.setdiff1d(all_dofs, constrained_dofs)
+
+        K_ff = K[np.ix_(free_dofs, free_dofs)]
+        Kg_ff = Kg[np.ix_(free_dofs, free_dofs)]
+
+        return PartitionedBucklingSystem(
+            K_ff=K_ff,
+            Kg_ff=Kg_ff,
             free_dofs=free_dofs,
             constrained_dofs=constrained_dofs,
         )
